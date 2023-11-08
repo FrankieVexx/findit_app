9d84b6164151f81851fd920edae58f9e5f3e0d9e
Francis Onyach <Franblog7@gmail.com>
2023-11-08 16:16:58 +0300

    added apps and created templates

--- findit/services/models.py
+++ findit/services/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Service(models.Model):
+    service_name = models.CharField(max_length=100)
+    service_type = models.CharField(max_length=100)
+    service_description = models.TextField(max_length=10000)
+    service_pub_date = models.DateTimeField(auto_created=True)
+
+
+
